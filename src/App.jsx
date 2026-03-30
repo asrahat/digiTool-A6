02d6f6ad7dab40e240843624fc22d391b2997e86
asrahat <asrahat4742@gmail.com>
2026-03-30 07:36:11 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Banner></Banner>
     </>
   );
 }
